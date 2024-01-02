04f4092ee1580cb657c0c453c067235f0f43b9fa
Brackly Murunga <khanbrackly@gmail.com>
2024-01-02 03:07:26 +0000

    request problem

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
-from flask import Flask
+from flask import Flask,request
 from flask_restful import Resource, Api
-from .src.ensers import QdrantVectorStore
-
+from src import ensers 
 app = Flask(__name__)
 api = Api(app)
 
+class Home(Resource):
+    def get(self):
+      return {'response': "Hi, there, thank your for using our NLP search tool; Ensers"}
+
+
 class Index(Resource):
-    def get(self,name_of_database,documents,fields):
-        store=QdrantVectorStore(name=name_of_database);
-        store.index_documents(documents,fields)
-        return {'hello': 'world'}
+    def post(self,name_of_database,documents,fields):
+        try:
+            store=ensers.QdrantVectorStore(name=name_of_database);
+            store.index_documents(documents,fields)
+            return {'response': 'success'}
+        except Exception as e:
+            return {'error': str(e)}
 
 class Search(Resource):
-    def get(self,name_of_database,querry):
-        store=QdrantVectorStore(name=name_of_database);
-        hits=store.search(querry)
-        return {'results': hits}
-
+    def post(self):
+        try:
+            name_of_database=request.form['data']['name_of_database']
+            print(request.form['data'])
+            querry=request.form['data']['querry']
+            store=ensers.QdrantVectorStore(name=name_of_database);
+            hits=store.search(querry)
+            return {'results': hits}
+        except Exception as e:
+            return {'error': str(e)}
+        
+api.add_resource(Home, '/')
 api.add_resource(Index, '/index')
 api.add_resource(Search, '/search')
 
