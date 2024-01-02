696dbb88e4e39389bb054c02c3c354ecf4e535b4
Brackly Murunga <khanbrackly@gmail.com>
2024-01-02 09:33:12 +0000

    working flask app

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask,request
-from flask_restful import Resource, Api
-from src import ensers 
+from flask_restful import Resource, Api,reqparse
+from ensers import vectorDB 
 app = Flask(__name__)
 api = Api(app)
 
@@ -10,22 +10,46 @@ class Home(Resource):
 
 
 class Index(Resource):
-    def post(self,name_of_database,documents,fields):
+    def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name_of_database', type=str, required=True, help='Name of the database')
+        parser.add_argument('q_drant_url', type=str, required=False, help='url of the database')
+        parser.add_argument('documents', type=list, required=True, help='List of documents')
+        parser.add_argument('fields', type=list, required=True, help='List of fields')
+
+        args = parser.parse_args()
+
         try:
-            store=ensers.QdrantVectorStore(name=name_of_database);
-            store.index_documents(documents,fields)
-            return {'response': 'success'}
+            args = request.get_json()
+
+            
+            name_of_database = args.get('name_of_database')
+            q_drant_url = args.get('q_drant_url')
+            documents = args.get('documents', [])
+            fields = args.get('fields', [])
+
+            try:
+                store = vectorDB.QdrantVectorStore(q_drant_url=q_drant_url,name=name_of_database)
+                res=store.index_documents(documents, fields)
+            except:
+                res=store.index_documents(documents, fields)
+            return {'response': res}
         except Exception as e:
             return {'error': str(e)}
 
+
 class Search(Resource):
     def post(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('name_of_database', type=str, required=True, help='Name of the database')
+        parser.add_argument('q_drant_url', type=str, required=False, help='url of the database')
+        parser.add_argument('use_memory', type=str, required=False, help='url of the database')
+        parser.add_argument('querry', type=str, required=True, help='term to search for')
+
+        args = parser.parse_args()
         try:
-            name_of_database=request.form['data']['name_of_database']
-            print(request.form['data'])
-            querry=request.form['data']['querry']
-            store=ensers.QdrantVectorStore(name=name_of_database);
-            hits=store.search(querry)
+            store = vectorDB.QdrantSearch(name=args['name_of_database'])
+            hits=store.search(args['querry'])
             return {'results': hits}
         except Exception as e:
             return {'error': str(e)}
---
+++ ensers/vectorDB.py
@@ -0,0 +1,69 @@
+import torch
+from sentence_transformers import SentenceTransformer
+from qdrant_client import models, QdrantClient
+
+encoder_name="sentence-transformers/all-MiniLM-L6-v2"
+qdrant_api="http://localhost:6333"
+memory_storage=":memory:"
+
+class QdrantVectorStore:
+
+    def __init__(self,
+                 name,
+                 q_drant_url=qdrant_api,
+                 encoder_name=encoder_name,
+                 use_Memory=False):
+        if use_Memory == False:
+            self.qdrant = QdrantClient(q_drant_url)
+        else:
+            self.qdrant = QdrantClient(memory_storage)
+
+        self.encoder=SentenceTransformer(encoder_name)
+        self.name =name
+
+        try:
+            self.qdrant.recreate_collection(
+                                                collection_name=self.name,
+                                                vectors_config=models.VectorParams(
+                                                    size=self.encoder.get_sentence_embedding_dimension(),
+                                                    distance=models.Distance.COSINE,
+                                                )
+                                            )
+        except Exception as e:
+            print(e)
+        
+    def index_documents(self,documents,fields):
+        try:
+            return self.qdrant.upload_records(
+                                                collection_name=self.name,
+                                                records=[
+                                                    models.Record(
+                                                        id=idx, vector=self.encoder.encode(str([f"{field}: {doc[field]}, " for field in fields]).replace("['",'').replace("]'",'')).tolist(), payload=doc
+                                                    )
+                                                    for idx, doc in enumerate(documents)
+                                                ],
+                                            )
+        except Exception as e:
+            return "error"+str(e)
+
+
+class QdrantSearch:
+    def __init__(self, name,encoder_name=encoder_name,q_drant_url=qdrant_api,use_Memory=False):
+        self.name = name
+        self.encoder = SentenceTransformer(encoder_name, device="cpu")
+
+        if use_Memory == False:
+            self.qdrant = QdrantClient(q_drant_url)
+        else:
+            self.qdrant = QdrantClient(memory_storage)
+
+    def search(self,querry):
+            try:
+                hits = self.qdrant.search(
+                    collection_name=self.name,
+                    query_vector=self.encoder.encode(querry).tolist(),
+                    limit=3,
+                    )
+                return [hit.payload for hit in hits]
+            except Exception as e:
+                return "error"+str(e)
